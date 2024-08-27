369db144016be6b909198dc4854764f4c8d3b91f
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-08-27 09:35:08 -0500

    update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,11 +1,11 @@
 'use strict';
 
-
 const { google } =require("googleapis");
 const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = ["https://lac1990.github.io/meet/"];
+
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
