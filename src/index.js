25481e87d1dbc715df7fe87d09d41ab0a439d705
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-09-05 08:53:09 -0500

    esbuild

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+const nodeJsFunction = new lambdaNodejs.NodejsFunction(
+  this,
+  "NodeJsFunction",
+  {
+    runtime: lambda.Runtime.NODEJS_16_X,
+    handler: "main",
+    entry: "../path/to/your/entry.js_or_ts",
+  }
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
