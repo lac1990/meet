db6c47f079504f84ec7cafd74e3950c0fccb7588
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-09-15 10:11:54 -0500

    update

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,30 +1,40 @@
+import { useState } from "react"
+import PropTypes from 'prop-types'
+
 const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
-    const handleInputChanged = (event) => {
-        const value = event.target.value;
+  const [number, setNumber] = useState(32)
 
-        if(isNaN(value) || value <= 0) {
-            setErrorAlert('Enter a valid number');
-        } else if (value > 32) {
-            setErrorAlert('Only maximum of 32 is allowed');
-        } else {
-            setErrorAlert('');
-            setCurrentNOE(value);
-        }
-    };
+  const handleInputChanged = (e) => {
+    let value = e.target.value
+    setNumber(value)
+    let errorText;
+    if(value === isNaN || value.length <= 0) { 
+      errorText = 'Please enter a valid number'
+    } else {
+      errorText = ''
+    }
+    setCurrentNOE(value)
+    setErrorAlert(errorText) 
+  }
+ 
+  return (
+    <div id="numberOfevents">
+      <label htmlFor="number" id="number">
+        Number of Events:
+      <input
+        type="text"
+        className="number"
+        value={number}
+        onChange={handleInputChanged}
+        />
+        </label>
+    </div> 
+  )
+}
 
-    return (
-        <div id="number-of-events">
-            <label>
-                Number of Events:
-            <input 
-            type="text"
-            defaultValue="32"
-            onChange={handleInputChanged}
-            data-testid="numberOfEventsInput"
-            />
-            </label>
-        </div>
-    )
-};
+export default NumberOfEvents
 
-export default NumberOfEvents;
+NumberOfEvents.propTypes = {
+  setCurrentNOE: PropTypes.func.isRequired,
+  setErrorAlert: PropTypes.func.isRequired
+}
