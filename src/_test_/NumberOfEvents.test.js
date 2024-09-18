04933916c5a68dfc170d62ea51d685899b9357ca
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-09-18 14:19:28 -0500

    up

--- src/_test_/NumberOfEvents.test.js
+++ src/_test_/NumberOfEvents.test.js
@@ -1,33 +1,35 @@
-import NumberOfEvents from '../components/NumberOfEvents';
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+/* eslint-disable testing-library/no-render-in-setup */
+/* eslint-disable testing-library/prefer-presence-queries */
+/* eslint-disable testing-library/prefer-screen-queries */
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event"; // Add this line to import the 'userEvent' library
+import NumberOfEvents from "../components/NumberOfEvents";
+import { getEvents } from "../api";
 
-describe('<NumberOfEvents /> Component', () => {
-    let NumberOfEventsComponent;
-    
-    beforeEach(() => {
-        NumberOfEventsComponent = render(
-            <NumberOfEvents 
-                setCurrentNOE={() => {}}
-                setErrorAlert={() => {}}
-            />
-        );
-    });
 
-    test('component contains input textbox', () => {
-        const input = NumberOfEventsComponent.queryByRole('textbox');
-        expect(input).toBeInTheDocument();
-    });
-    
-    test('ensures the default value of textbox is 32', () => {
-        const input = NumberOfEventsComponent.queryByRole('textbox');
-        expect(input).toHaveValue('32');
-    });
+describe('<NumberOfEvents /> component', () => { 
+  let NumberOfEventsComponent;
+  beforeEach(() => {
+    NumberOfEventsComponent = render(<NumberOfEvents setCurrentNOE={() => {}} setErrorAlert={() => {}} />);
+  });
+ 
+  test('render element with role of textbox', () => { 
+    const input = NumberOfEventsComponent.queryByRole('textbox');
+    expect(input).toBeInTheDocument(); 
+  });
 
-    test('textbox value changes when user updates input', async () => {
-        const input = NumberOfEventsComponent.getByTestId('numberOfEventsInput');
-        const user = userEvent.setup();
-        await user.type(input, '{backspace}{backspace}10');
-        expect(input).toHaveValue('10');
-    })
-})
+
+  test('default number of events is 32', () => {
+    expect( NumberOfEventsComponent.getByRole('textbox')).toHaveValue('32');  
+  });  
+
+  test('change number of events when a user types in the textbox', async () => { 
+    const numverOfEvents = NumberOfEventsComponent.getByRole('textbox');
+    const user = userEvent.setup(); 
+    await user.type(numverOfEvents, '{backspace}{backspace}10');   
+    const allEvents = await getEvents(); 
+    NumberOfEventsComponent.rerender(<NumberOfEvents setCurrentNOE={allEvents} setErrorAlert={() => {}} />);   
+    expect(numverOfEvents).toHaveValue('10'); 
+  }); 
+});  
+ 
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,8 +2,9 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// src/setupTests.js
 
+import '@testing-library/jest-dom';
 
 // Here, add portions of the warning messages you want to intentionally prevent from appearing
 const MESSAGES_TO_IGNORE = [
@@ -17,6 +18,23 @@ const originalError = console.error.bind(console.error);
 console.error = (...args) => {
   const ignoreMessage = MESSAGES_TO_IGNORE.find(message => args.toString().includes(message));
   if (!ignoreMessage) originalError(...args);
-} 
+}
 
-jest.setTimeout(30000);
+jest.setTimeout(1000000);
+
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+  //@ts-ignore
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
